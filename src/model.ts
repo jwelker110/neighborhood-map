557b03b9ef508b6b0e94006b24bb5c691329a4b1
Jon Welker <JWelker110@gmail.com>
2016-07-16 17:51:03 -0500

    Remove controller logic from view model

--- src/model.ts
+++ src/model.ts
@@ -4,66 +4,32 @@ export class ViewModel {
 
     locations: KnockoutObservableArray<any> = ko.observableArray([]);
     filteredLocations: KnockoutObservableArray<any> = ko.observableArray([]);
-    filter: KnockoutObservable<string> = ko.observable('');
-    currentLocation: KnockoutObservable<any> = ko.observable();
-    info: KnockoutObservable<string> = ko.observable('');
+    filter: string = '';
     map: any;
 
     constructor(map: any){
         this.map = map;
-        this.getNearbyLocations();
+        this.map.search(undefined, this.searchCallback);
     }
 
     setLocations = (locations: any[]) => {
-        // TODO check for empty and display message
-        for(let i = 0, l = locations.length; i < l; i++) {
-            locations[i].marker = this.map.addMarker(locations[i]);
-            locations[i].isMatched = ko.observable(true);
-        }
         this.locations(locations);
+        this.setFilteredLocations(locations);
+    };
+
+    setFilteredLocations = (locations: any[]) => {
         this.filteredLocations(locations);
     };
 
-    /**
-     * Remove the event listeners we no longer need
-     */
-    removeLocations = () => {
-        this.locations = ko.observableArray([]);
+    searchCallback = (results: any[]) => {
+        this.setLocations(results);
     };
 
     setCurrentLocation = (place: any) => {
-        this.currentLocation(place);
-        this.map.gMap.panTo(place.geometry.location);
-        this.map.animateMarker(place.marker);
-        this.map.setInfoWindow(place.name, place.marker);
-    };
-
-    getNearbyLocations = () => {
-        this.map.nearbySearch(this.setNearbyLocations);
-    };
-
-    setNearbyLocations = (results: any[], status: any) => {
-        if(status == google.maps.places.PlacesServiceStatus.OK){
-            var markers = this.locations();
-            for(let i = 0, l = markers.length; i < l; i++) {
-                this.map.removeMarker(markers[i]);
-            }
-            this.setLocations(results);
-        }
+        this.map.triggerMarker(place.marker);
     };
 
     filterLocations = (obj: any, event: any) => {
-        var value = event.target.value.toLowerCase();
-        this.filteredLocations(this.locations().filter((place: any) => {
-            var match = place.name.toLowerCase().indexOf(value) > -1;
-            if(!match) {
-                place.marker.setMap(null);
-                return match;
-            }
-            if(!place.marker.map) {
-                place.marker.setMap(this.map.gMap);
-            }
-            return match;
-        }));
+        this.setLocations(this.map.filterLocations(obj.filter));
     };
 }
