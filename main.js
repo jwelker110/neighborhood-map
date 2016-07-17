c0066d483c53b8e0a58ec9b14328cb4e908dbfb6
Jon Welker <JWelker110@gmail.com>
2016-07-17 17:00:11 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,6 @@
+define(["require", "exports", "knockout", "model"], function (require, exports, ko, model_1) {
+    "use strict";
+    // create our model here
+    var vm = new model_1.ViewModel(map);
+    ko.applyBindings(vm);
+});
