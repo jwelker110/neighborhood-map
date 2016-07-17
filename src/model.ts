4aa76c5bc0fe48ca7326187bdb1954ab7c4c9108
Jon Welker <JWelker110@gmail.com>
2016-07-17 14:34:41 -0500

    Add list collapsed var and computed classes

--- src/model.ts
+++ src/model.ts
@@ -2,15 +2,32 @@ import * as ko from "knockout";
 
 export class ViewModel {
 
-    locations: KnockoutObservableArray<any> = ko.observableArray([]);
-    filteredLocations: KnockoutObservableArray<any> = ko.observableArray([]);
-    filter: string = '';
+    locations = ko.observableArray([]);
+    filteredLocations = ko.observableArray([]);
+    filter = '';
     map: any;
+    isListCollapsed = ko.observable(true);
+    isCollapsedComputed: KnockoutComputed<any>;
+    isPlusComputed: KnockoutComputed<any>;
 
     constructor(map: any){
         this.map = map;
         this.map.search(undefined, this.searchCallback);
+        this.isCollapsedComputed = ko.pureComputed(() => {
+            return this.isListCollapsed() ? 'ptm-list-collapsed' : 'ptm-list-expanded';
+        }, this);
+        this.isPlusComputed = ko.pureComputed(() => {
+            return this.isListCollapsed() ? 'ptm-expanded' : 'ptm-collapsed';
+        });
     }
+
+    toggleListCollapsed = () => {
+        this.isListCollapsed(!this.isListCollapsed());
+    };
+
+    expandList = () => {
+        this.isListCollapsed(false);
+    };
 
     setLocations = (locations: any[]) => {
         this.locations(locations);
@@ -26,6 +43,7 @@ export class ViewModel {
     };
 
     setCurrentLocation = (place: any) => {
+        this.isListCollapsed(true);
         this.map.triggerMarker(place.marker);
     };
 
