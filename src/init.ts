9723fa8bc02d92084a7f6a99d94dd16512b6bfdd
Jon Welker <JWelker110@gmail.com>
2016-08-13 08:16:17 -0500

    Initial Commit

---
+++ src/init.ts
@@ -0,0 +1,9 @@
+/**
+ * function req'd by Google Maps to begin the initialization process
+ * of the map
+ */
+var map: any;
+declare var google: any;
+function initMap(){
+    map = new GoogleMap(new Coords(30.4583, -91.1403), undefined, 12);
+}
