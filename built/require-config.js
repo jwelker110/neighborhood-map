5581e868c4f7c126a379392a90483273342f2e05
Jon Welker <JWelker110@gmail.com>
2016-08-28 15:55:09 -0500

    Update path for knockout for master branch

--- built/require-config.js
+++ built/require-config.js
@@ -1,6 +1,6 @@
 require.config({
     paths: {
-        "knockout": "/neighborhood-map/externals/knockout.min"
+        "knockout": "../externals/knockout.min"
     }
 });
 //# sourceMappingURL=require-config.js.map
--- src/require-config.ts
+++ src/require-config.ts
@@ -1,6 +1,6 @@
 declare var require: any;
 require.config({
     paths: {
-        "knockout": "/neighborhood-map/externals/knockout.min"
+        "knockout": "../externals/knockout.min"
     }
 });
