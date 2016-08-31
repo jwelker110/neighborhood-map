b700664b423ae6a71ec6b5ff4bf2b4a02b2ffd25
Jon Welker <JWelker110@gmail.com>
2016-08-31 05:35:42 -0500

    Use main to load necessary config and setup callback for gmaps

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,30 @@
-import * as ko from "knockout";
-import {ViewModel} from "model";
+declare var require: any;
+require.config({
+    paths: {
+        "knockout": "../externals/knockout.min"
+    }
+});
 
-// create our model here
-var vm = new ViewModel(map);
+var map: any;
+var google: any;
 
-ko.applyBindings(vm);
+function initMap() {
+    /**
+     * setting a timeout so that we can get the map created once
+     * it has loaded.
+     * doing this because with the modules in TS it has been extremely
+     * difficult to set initMap on the global scope so I am including it
+     * within index so that google map api may call it.
+     */
+    require( ['knockout', 'model', 'googlemap'], function(ko: any, model: any, googlemap: any) {
+        map = new googlemap.GoogleMap(new googlemap.Coords(30.4583, -91.1403), undefined, 13);
+        var vm = new model.ViewModel(map);
+        ko.applyBindings(vm);
+    });
+
+}
+
+function onMapError(err: any) {
+    console.log('an error occurred');
+}
+
