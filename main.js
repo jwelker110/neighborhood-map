5cb3367a87c7b3af40378bffaf67e563e79f2735
Jon Welker <JWelker110@gmail.com>
2016-08-31 05:36:51 -0500

    Rebuild to reflect changes in src

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-define(["require", "exports", "knockout", "model"], function (require, exports, ko, model_1) {
-    "use strict";
-    // create our model here
-    var vm = new model_1.ViewModel(map);
-    ko.applyBindings(vm);
+require.config({
+    paths: {
+        "knockout": "../externals/knockout.min"
+    }
 });
+var map;
+var google;
+function initMap() {
+    /**
+     * setting a timeout so that we can get the map created once
+     * it has loaded.
+     * doing this because with the modules in TS it has been extremely
+     * difficult to set initMap on the global scope so I am including it
+     * within index so that google map api may call it.
+     */
+    require(['knockout', 'model', 'googlemap'], function (ko, model, googlemap) {
+        map = new googlemap.GoogleMap(new googlemap.Coords(30.4583, -91.1403), undefined, 13);
+        var vm = new model.ViewModel(map);
+        ko.applyBindings(vm);
+    });
+}
+function onMapError(err) {
+    console.log('an error occurred');
+}
 //# sourceMappingURL=main.js.map
