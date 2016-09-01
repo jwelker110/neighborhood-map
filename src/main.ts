0208be2155d6d1a39789b48f6c392db0e2bb55b6
Jon Welker <JWelker110@gmail.com>
2016-09-01 03:53:18 -0500

    Update comment and spacing

--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,19 @@ require.config({
 var map: any;
 var google: any;
 
+/**
+ * Setting up the map and the Knockout bindings. Done in here so we know the
+ * Google map is ready to go before we try to use it.
+ */
 function initMap() {
-    /**
-     * setting a timeout so that we can get the map created once
-     * it has loaded.
-     * doing this because with the modules in TS it has been extremely
-     * difficult to set initMap on the global scope so I am including it
-     * within index so that google map api may call it.
-     */
     require( ['knockout', 'model', 'googlemap'], function(ko: any, model: any, googlemap: any) {
+
         map = new googlemap.GoogleMap(new googlemap.Coords(30.4583, -91.1403), undefined, 13);
+
         var vm = new model.ViewModel(map);
+
         ko.applyBindings(vm);
     });
-
 }
 
 function onMapError(err: any) {
